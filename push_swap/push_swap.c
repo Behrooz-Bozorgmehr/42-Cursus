1fa0988c5ae8123e757d78a0e22d89a01487ca79
Behrooz Bozorgmehr <bozorjmehr@gmail.com>
2022-06-19 19:21:19 +0200

    Version 1 main.c cleaned

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: bbozorgm <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 17:31:08 by bbozorgm          #+#    #+#             */
-/*   Updated: 2022/06/19 19:01:06 by bbozorgm         ###   ########.fr       */
+/*   Updated: 2022/06/19 19:15:28 by bbozorgm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,31 +41,7 @@ int	main(int argc, char *argv[])
 	t_stack	*b;
 	a = init(argc - 1, argv + 1);
 	b = NULL;
-/*	char *ptr[] = {"25", "-25"};
-	b = init(2, ptr);	
-	printf("sorted : %d\n", check_order(a));
-	print(a, b);
-	printf("size a: %d\t size b: %d\n", lst_size(a), lst_size(b));
-
-	pop_push(&a, &b);
-	printf("size a: %d\t size b: %d\n", lst_size(a), lst_size(b));
-	print(a, b);
-
-	pop_push(&a, &b);
-	printf("size a: %d\t size b: %d\n", lst_size(a), lst_size(b));
-	print(a, b);
-
-*/	
 	sort(a,b);
 
-/*
-
-	while(a->next != NULL)
-	{
-		printf("val: %d\t next: %d\t prev : %d\n", a->val, a->next->val, a->prev->val);
-		 a= a->next;
-	}
-	printf("val: %d\t next: %p\t prev : %d\n", a->val, a->next, a->prev->val);
-	a  = init(argc, argv);
-*/		return (0);
+	return (0);
 }
